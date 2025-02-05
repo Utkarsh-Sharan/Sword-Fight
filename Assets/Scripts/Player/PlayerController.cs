5d29dbdb3d03404a132407a9508cd310c6404f6d
Utkarsh Sharan <utkarsh.sharan99@gmail.com>
2025-02-05 07:25:41 +0530

    added player movement

--- Assets/Scripts/Player/PlayerController.cs
+++ Assets/Scripts/Player/PlayerController.cs
@@ -4,15 +4,36 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    [SerializeField] private CharacterController characterController;
+
+    private float horizontalInput;
+    private float verticalInput;
+    private float moveSpeed = 5f;
+    private Vector3 movementVelocity;
+
+    private void FixedUpdate()
     {
-        
+        CalculateMovement();
+        characterController.Move(movementVelocity);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void CalculateMovement()
     {
-        
+        movementVelocity.Set(horizontalInput, 0, verticalInput);
+        movementVelocity.Normalize();
+        movementVelocity = Quaternion.Euler(0, -45f, 0) * movementVelocity;
+        movementVelocity *= moveSpeed * Time.deltaTime;
+    }
+
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal");
+        verticalInput = Input.GetAxisRaw("Vertical");
+    }
+
+    private void OnDisable()
+    {
+        horizontalInput = 0f;
+        verticalInput = 0f;
     }
 }
